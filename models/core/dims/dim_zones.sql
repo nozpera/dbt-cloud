b9c5b1977122b42d65f76399f54445782c6138ef
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2025-03-12 22:35:04 +0700

    Removed all files

--- models/core/dims/dim_zones.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    locationid,
-    borough,
-    zone,
-    replace(service_zone, 'Boro', 'Green') as service_zone
-FROM {{ ref('taxi_zone_lookup') }}
--- note/model_to_generate_syntax.sql
+++
@@ -1,4 +0,0 @@
-{% set models_to_generate = codegen.get_models(directory='core/facts', prefix='fact_') %}
-{{ codegen.generate_model_yaml(
-    model_names = models_to_generate
-) }}
